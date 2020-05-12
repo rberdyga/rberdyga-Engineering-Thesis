d79ebb956273fe7b94674f0fe6bafacff8d8068d
rberdyga <rberdyga@gmail.com>
2020-05-12 14:41:52 +0200

    Added dcmtk libraries correctly

--- App/sources/main.cpp
+++ App/sources/main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 
+#include "dcmtk/dcmimgle/dcmimage.h"
+#include "dcmtk/dcmimgle/dipixel.h"
+#include "dcmtk/dcmimgle/diimage.h"
+#include "dcmtk/dcmimgle/dimo1img.h"
+#include "dcmtk/dcmimgle/dimo2img.h"
+#include "dcmtk/dcmdata/dctk.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    auto *image = new DicomImage("test.dcm");
+
+    std::cout << "hell yeah!";
+
     MainWindow w;
     w.show();
-    return a.exec();
+    return QApplication::exec();
 }
